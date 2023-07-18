7a5e04b7c6ca6c4ab38fdc152c90f222c2fea4c4
Hesam <61893006+hesam1993@users.noreply.github.com>
2023-07-18 20:11:00 +0000

    feat: adding users functionality, in users.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,37 +1,41 @@
-const socket = io()
-const sendBtn = document.querySelector('#send-message')
-const message = document.querySelector('#message')
-const messages = document.querySelector('#messages')
+const socket = io();
+const sendBtn = document.querySelector("#send-message");
+const message = document.querySelector("#message");
+const messages = document.querySelector("#messages");
 
 //Options
-const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
+socket.on("server-message", (message) => {
+  console.log(message);
+  const messageEl = document.createElement("p");
+  messageEl.innerHTML = message;
+  messages.insertAdjacentElement("beforeend", messageEl);
+  resetMessageForm();
+});
 
+socket.on("roomData", (roomData) => {
+  console.log(roomData);
+});
 
-socket.on('server-message', (message) => {
-    console.log(message)
-    const messageEl = document.createElement('p')
-    messageEl.innerHTML = message
-    messages.insertAdjacentElement('beforeend', messageEl)
-    resetMessageForm()
-})
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  socket.emit("client-message", message.value, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message is received by the server");
+  });
+});
 
-sendBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    socket.emit('client-message', message.value, (error)=>{
-        if(error){
-            return console.log(error)
-        }
-        console.log('Message is received by the server')
-    })
-})
-
-
-socket.emit('join', {username, room}, (error) => {
-    console.log(error)
-},)
+socket.emit("join", { username, room }, (error) => {
+  alert(error);
+  location.href = "/";
+});
 
 const resetMessageForm = () => {
-    message.value = ''
-    message.focus()
-}
+  message.value = "";
+  message.focus();
+};
--- src/index.js
+++ src/index.js
@@ -1,44 +1,53 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const { addUser, getUsersInRoom } = require("./utils/users");
 
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+  socket.on("join", ({ username, room }, callback) => {
+    console.log(username, room);
+    const { error, user } = addUser({ id: socket.id, username, room });
+    if (error) {
+      return callback(error);
+    }
 
-    socket.on('join', ({ username, room }, callback) => {
-        console.log(username, room)
-        socket.emit('server-message', `Welcome ${username}`)
-        socket.broadcast.emit('server-message', 'a new user is joined')
-    })
+    socket.join(user.room);
+    socket.emit("server-message", `Welcome ${username}`);
+    socket.broadcast
+      .to(user.room)
+      .emit("server-message", `${user.username} has joined the room!`);
 
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+  });
 
-    
-    //sending client message to everyone in the room
-    socket.on('client-message', (message, callback) => {
-        io.emit('server-message', message)
-        callback()
-    })
+  //sending client message to everyone in the room
+  socket.on("client-message", (message, callback) => {
+    io.emit("server-message", message);
+    callback();
+  });
 
-    //informing everyone that someone has left the room
-    socket.on('disconnect', () => {
-        io.emit('server-message', 'a user has left')
-    })
-})
-
+  //informing everyone that someone has left the room
+  socket.on("disconnect", () => {
+    io.emit("server-message", "a user has left");
+  });
+});
 
 server.listen(port, () => {
-    console.log('Server is running on port ' + port)
-})
+  console.log("Server is running on port " + port);
+});
